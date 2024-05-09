e8befbdd63dfc40ab11ce593b71e2fafaaa42a8a
NotNamedReverse <151482196+NotNamedReverse@users.noreply.github.com>
2024-05-09 07:40:24 -0400

    Update requestAttack.js

--- requestAttack.js
+++ requestAttack.js
@@ -2,5 +2,5 @@ const requestButton = document.getElementById("RequestButton");
 
 requestButton.onclick = function relocate()
 {
-    window.location.href = "/requstAttack.html";
+    window.location.href = "https://notnamedreverse.github.io/requestattack/";
 }
